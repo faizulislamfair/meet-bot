22d5c085311df09fe64b1716f32a206ed206740a
faizulislamfair <faizulislamfair@gmail.com>
2023-10-28 13:01:38 +0600

    first bot

---
+++ meet_bot.py
@@ -0,0 +1,25 @@
+from time import sleep
+import pyautogui as auto
+import schedule, webbrowser
+
+
+link = "meet.google.com/yia-ckmq-mtc"
+
+time = "13:00"
+
+def join():
+    webbrowser.open_new_tab('https://' + link)
+    sleep(7)
+    auto.hotkey('ctrl', 'd')
+    auto.hotkey('ctrl', 'e')
+    auto.click(1352, 541)
+
+
+schedule.every().day.at(time).do(join)
+
+while True:
+    schedule.run_pending()
+    sleep(1)
+
+
+
